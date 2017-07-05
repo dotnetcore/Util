d9e2ef0b766a99f09efeddf09ae457c88b33da51
ºÎÕòϫ <8907060@qq.com>
2017-07-06 01:06:22 +0800

    1. 添加Util项目及单元测试项目 2. 添加转换操作类及单元测试

---
+++ src/Util/Helpers/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Util.Helpers {
+    /// <summary>
+    /// 常用公共操作
+    /// </summary>
+    public static class Common {
+        /// <summary>
+        /// 获取类型
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        public static Type GetType<T>() {
+            var type = typeof( T );
+            return Nullable.GetUnderlyingType( type ) ?? type;
+        }
+    }
+}
