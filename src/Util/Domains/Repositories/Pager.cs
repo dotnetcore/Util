9d7b155eb1113e2d380dcec6f8bb788c8ac62958
utilcore <8907060@qq.com>
2017-08-06 09:54:18 +0800

    更新了分页参数与分页集合

---
+++ src/Util/Domains/Repositories/Pager.cs
@@ -0,0 +1,139 @@
+﻿using System.Text;
+
+namespace Util.Domains.Repositories {
+    /// <summary>
+    /// 分页参数
+    /// </summary>
+    public class Pager : IPager {
+        /// <summary>
+        /// 初始化分页参数
+        /// </summary>
+        public Pager()
+            : this( 1 ) {
+        }
+
+        /// <summary>
+        /// 初始化分页参数
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="pageSize">每页显示行数,默认20</param> 
+        /// <param name="order">排序条件</param>
+        public Pager( int page, int pageSize, string order )
+            : this( page, pageSize, 0, order ) {
+        }
+
+        /// <summary>
+        /// 初始化分页参数
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="pageSize">每页显示行数,默认20</param> 
+        /// <param name="totalCount">总行数</param>
+        /// <param name="order">排序条件</param>
+        public Pager( int page, int pageSize = 20, int totalCount = 0, string order = "" ) {
+            Page = page;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+            Order = order;
+        }
+
+        private int _pageIndex;
+        /// <summary>
+        /// 页索引，即第几页，从1开始
+        /// </summary>
+        public int Page {
+            get {
+                if( _pageIndex <= 0 )
+                    _pageIndex = 1;
+                return _pageIndex;
+            }
+            set { _pageIndex = value; }
+        }
+
+        /// <summary>
+        /// 每页显示行数
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 总行数
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// 获取总页数
+        /// </summary>
+        public int GetPageCount() {
+            if ( ( TotalCount % PageSize ) == 0 )
+                return TotalCount / PageSize;
+            return ( TotalCount / PageSize ) + 1;
+        }
+
+        /// <summary>
+        /// 获取跳过的行数
+        /// </summary>
+        public int GetSkipCount() {
+            if ( Page > GetPageCount() )
+                Page = GetPageCount();
+            return PageSize * ( Page - 1 );
+        }
+
+        /// <summary>
+        /// 排序条件
+        /// </summary>
+        public string Order { get; set; }
+
+        /// <summary>
+        /// 起始行数
+        /// </summary>
+        public int GetStartNumber() {
+            return ( Page - 1 ) * PageSize + 1;
+        }
+        /// <summary>
+        /// 结束行数
+        /// </summary>
+        public int GetEndNumber() {
+            return Page * PageSize;
+        }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        private StringBuilder _description;
+
+        /// <summary>
+        /// 输出对象状态
+        /// </summary>
+        public override string ToString() {
+            _description = new StringBuilder();
+            AddDescriptions();
+            return _description.ToString().TrimEnd().TrimEnd( ',' );
+        }
+
+        /// <summary>
+        /// 添加描述
+        /// </summary>
+        protected virtual void AddDescriptions() {
+            AddDescription( "Page", Page );
+            AddDescription( "PageSize", PageSize );
+            AddDescription( "Order", Order );
+        }
+
+        /// <summary>
+        /// 添加描述
+        /// </summary>
+        protected void AddDescription( string description ) {
+            if( string.IsNullOrWhiteSpace( description ) )
+                return;
+            _description.Append( description );
+        }
+
+        /// <summary>
+        /// 添加描述
+        /// </summary>
+        protected void AddDescription<TValue>( string name, TValue value ) {
+            if( string.IsNullOrWhiteSpace( value.SafeString() ) )
+                return;
+            _description.AppendFormat( "{0}:{1},", name, value );
+        }
+    }
+}
---
+++ src/Util/Domains/Repositories/PagerList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Util.Domains.Repositories {
+    /// <summary>
+    /// 分页集合
+    /// </summary>
+    /// <typeparam name="T">元素类型</typeparam>
+    [Serializable]
+    public class PagerList<T> : IPagerBase {
+        /// <summary>
+        /// 初始化分页集合
+        /// </summary>
+        public PagerList() : this( 0 ) {
+        }
+
+        /// <summary>
+        /// 初始化分页集合
+        /// </summary>
+        /// <param name="totalCount">总行数</param>
+        public PagerList( int totalCount )
+            : this( 1, 20, totalCount ) {
+        }
+
+        /// <summary>
+        /// 初始化分页集合
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="pageSize">每页显示行数</param>
+        /// <param name="totalCount">总行数</param>
+        public PagerList( int page, int pageSize, int totalCount )
+            : this( page, pageSize, totalCount, "" ) {
+        }
+
+        /// <summary>
+        /// 初始化分页集合
+        /// </summary>
+        /// <param name="page">页索引</param>
+        /// <param name="pageSize">每页显示行数</param>
+        /// <param name="totalCount">总行数</param>
+        /// <param name="order">排序条件</param>
+        public PagerList( int page, int pageSize, int totalCount, string order ) {
+            Data = new List<T>();
+            var pager = new Pager( page, pageSize, totalCount );
+            TotalCount = pager.TotalCount;
+            PageCount = pager.GetPageCount();
+            Page = pager.Page;
+            PageSize = pager.PageSize;
+            Order = order;
+        }
+
+        /// <summary>
+        /// 初始化分页集合
+        /// </summary>
+        /// <param name="pager">查询对象</param>
+        public PagerList( IPager pager )
+            : this( pager.Page, pager.PageSize, pager.TotalCount, pager.Order ) {
+        }
+
+        /// <summary>
+        /// 页索引，即第几页，从1开始
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        /// 每页显示行数
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 总行数
+        /// </summary>
+        public int TotalCount { get; set; }
+
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        public int PageCount { get; set; }
+
+        /// <summary>
+        /// 排序条件
+        /// </summary>
+        public string Order { get; set; }
+
+        /// <summary>
+        /// 数量
+        /// </summary>
+        public int Count => Data.Count;
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public List<T> Data { get; set; }
+
+        /// <summary>
+        /// 索引器
+        /// </summary>
+        /// <param name="index">索引</param>
+        public T this[ int index ] {
+            get => Data[index];
+            set => Data[index] = value;
+        }
+
+        /// <summary>
+        /// 添加元素
+        /// </summary>
+        /// <param name="item">元素</param>
+        public void Add( T item ) {
+            Data.Add( item );
+        }
+
+        /// <summary>
+        /// 添加元素集合
+        /// </summary>
+        /// <param name="collection">元素集合</param>
+        public void AddRange( IEnumerable<T> collection ) {
+            Data.AddRange( collection );
+        }
+
+        /// <summary>
+        /// 清空
+        /// </summary>
+        public void Clear() {
+            Data.Clear();
+        }
+
+        /// <summary>
+        /// 转换分页集合的元素类型
+        /// </summary>
+        /// <typeparam name="TResult">目标元素类型</typeparam>
+        /// <param name="converter">转换方法</param>
+        public PagerList<TResult> Convert<TResult>( Func<T, TResult> converter ) {
+            var result = new PagerList<TResult>( Page, PageSize, TotalCount, Order );
+            result.AddRange( this.Data.Select( converter ) );
+            return result;
+        }
+    }
+}
