e70b73e676fd813581d98fc88b68801ad45fbcb7
Lemon <liuhaoyang1221@hotmail.com>
2017-08-06 23:40:45 +0800

    Add benchmark for Util.Helpers.Convert.ToInt

---
+++ performance/Util.Performance/Benchmarks/ConvertBenchmarks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Jobs;
+
+namespace Util.Performance.Benchmarks
+{
+    [MemoryDiagnoser]
+    [AllStatisticsColumn]
+    public class ConvertBenchmarks
+    {
+        [Benchmark(Description = "Util.Helpers.Convert.ToInt")]
+        public int ToInt()
+        {
+            return Util.Helpers.Convert.ToInt("1A");
+        }
+    }
+}
