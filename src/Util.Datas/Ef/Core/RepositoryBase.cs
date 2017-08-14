f91f1e89e3045e66a47b032a60467db5c12748f6
utilcore <8907060@qq.com>
2017-08-14 14:18:03 +0800

    重构仓储基类结构

--- src/Util.Datas/Ef/Core/RepositoryBase.cs
+++ src/Util.Datas/Ef/Core/RepositoryBase.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Util.Datas.Ef.Internal;
 using Util.Domains;
 using Util.Domains.Repositories;
@@ -9,7 +12,7 @@ namespace Util.Datas.Ef.Core {
     /// 仓储
     /// </summary>
     /// <typeparam name="TEntity">实体类型</typeparam>
-    public abstract class RepositoryBase<TEntity> : RepositoryBase<TEntity, Guid>, IRepository<TEntity>
+    public abstract class RepositoryBase<TEntity> : RepositoryBase<TEntity, Guid>, IRepository<TEntity>, IReadableRepository<TEntity>
         where TEntity : class, IAggregateRoot<TEntity, Guid> {
         /// <summary>
         /// 初始化仓储
@@ -25,7 +28,7 @@ namespace Util.Datas.Ef.Core {
     /// </summary>
     /// <typeparam name="TEntity">实体类型</typeparam>
     /// <typeparam name="TKey">实体标识类型</typeparam>
-    public abstract class RepositoryBase<TEntity, TKey> : ReadableRepositoryBase<TEntity, TKey>, IRepository<TEntity, TKey>
+    public abstract class RepositoryBase<TEntity, TKey> : IRepository<TEntity, TKey>
         where TEntity : class, IAggregateRoot<TEntity, TKey> {
         /// <summary>
         /// 数据上下文包装器
@@ -36,8 +39,59 @@ namespace Util.Datas.Ef.Core {
         /// 初始化仓储
         /// </summary>
         /// <param name="unitOfWork">工作单元</param>
-        protected RepositoryBase( IUnitOfWork unitOfWork ) : base( unitOfWork ) {
+        protected RepositoryBase( IUnitOfWork unitOfWork ) {
             _wrapper = new DbContextWrapper<TEntity, TKey>( unitOfWork );
+            UnitOfWork = _wrapper.UnitOfWork;
+            Set = _wrapper.Set;
+        }
+
+        /// <summary>
+        /// 工作单元
+        /// </summary>
+        protected UnitOfWorkBase UnitOfWork { get; }
+
+        /// <summary>
+        /// 实体集
+        /// </summary>
+        protected DbSet<TEntity> Set { get; }
+
+        /// <summary>
+        /// 查找实体
+        /// </summary>
+        public IQueryable<TEntity> Find() {
+            return _wrapper.Find();
+        }
+
+        /// <summary>
+        /// 查找实体
+        /// </summary>
+        /// <param name="id">实体标识</param>
+        public TEntity Find( object id ) {
+            return _wrapper.Find( id );
+        }
+
+        /// <summary>
+        /// 查找实体
+        /// </summary>
+        /// <param name="id">实体标识</param>
+        public async Task<TEntity> FindAsync( object id ) {
+            return await _wrapper.FindAsync( id );
+        }
+
+        /// <summary>
+        /// 获取单个实体
+        /// </summary>
+        /// <param name="predicate">条件</param>
+        public TEntity Single( Expression<Func<TEntity, bool>> predicate ) {
+            return _wrapper.Single( predicate );
+        }
+
+        /// <summary>
+        /// 获取单个实体
+        /// </summary>
+        /// <param name="predicate">查询条件</param>
+        public async Task<TEntity> SingleAsync( Expression<Func<TEntity, bool>> predicate ) {
+            return await _wrapper.SingleAsync( predicate );
         }
 
         /// <summary>
