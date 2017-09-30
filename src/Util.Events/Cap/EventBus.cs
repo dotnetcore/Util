8667da5e12f3f723f5a5f8dbefa870cfd7ac7c44
utilcore <8907060@qq.com>
2017-09-30 23:56:28 +0800

    集成Cap事件总线

---
+++ src/Util.Events/Cap/EventBus.cs
@@ -0,0 +1,32 @@
+﻿using DotNetCore.CAP;
+
+namespace Util.Events.Cap {
+    /// <summary>
+    /// Cap事件总线
+    /// </summary>
+    public class EventBus : IEventBus {
+        /// <summary>
+        /// Cap事件发布器
+        /// </summary>
+        private readonly ICapPublisher _publisher;
+
+        /// <summary>
+        /// 初始化Cap事件总线
+        /// </summary>
+        /// <param name="publisher">Cap事件发布器</param>
+        public EventBus( ICapPublisher publisher ) {
+            _publisher = publisher;
+        }
+
+        /// <summary>
+        /// 发布事件
+        /// </summary>
+        /// <typeparam name="TEvent">事件类型</typeparam>
+        /// <param name="event">事件</param>
+        public void Publish<TEvent>( TEvent @event ) where TEvent : IEvent {
+            if ( !( @event is ICapEvent capEvent ) )
+                return;
+            _publisher.Publish( capEvent.Name, capEvent.Content, capEvent.Callback );
+        }
+    }
+}
---
+++ src/Util.Events/Cap/ICapEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace Util.Events.Cap {
+    /// <summary>
+    /// Cap事件
+    /// </summary>
+    public interface ICapEvent : IEvent {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        object Content { get; set; }
+        /// <summary>
+        /// 回调
+        /// </summary>
+        string Callback { get; set; }
+    }
+}
