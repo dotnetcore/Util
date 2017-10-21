568af8a8e6a23d4733922de26d003c60fa748494
utilcore <8907060@qq.com>
2017-10-21 19:47:21 +0800

    修复AOP导致工作单元失败

--- src/Util/Datas/UnitOfWorks/IUnitOfWork.cs
+++ src/Util/Datas/UnitOfWorks/IUnitOfWork.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Util.Aspects;
 
 namespace Util.Datas.UnitOfWorks {
     /// <summary>
     /// 工作单元
     /// </summary>
-    [Ignore]
     public interface IUnitOfWork : IDisposable {
         /// <summary>
         /// 提交,返回影响的行数
