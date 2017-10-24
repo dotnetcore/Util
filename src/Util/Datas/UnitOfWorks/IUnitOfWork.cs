557a1199744fae6d6b03af28b242bcbc28e3f650
utilcore <8907060@qq.com>
2017-10-24 08:43:49 +0800

    更新工作单元，忽略创建动态代理

--- src/Util/Datas/UnitOfWorks/IUnitOfWork.cs
+++ src/Util/Datas/UnitOfWorks/IUnitOfWork.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Util.Aspects;
 
 namespace Util.Datas.UnitOfWorks {
     /// <summary>
     /// 工作单元
     /// </summary>
+    [Ignore]
     public interface IUnitOfWork : IDisposable {
         /// <summary>
         /// 提交,返回影响的行数
--- test/Util.Datas.Tests.Integration/Samples/Datas/SqlServer/UnitOfWorks/SqlServerUnitOfWork.cs
+++ test/Util.Datas.Tests.Integration/Samples/Datas/SqlServer/UnitOfWorks/SqlServerUnitOfWork.cs
@@ -1,12 +1,10 @@
-﻿using Util.Aspects;
-using Util.Datas.Tests.SqlServer.Configs;
+﻿using Util.Datas.Tests.SqlServer.Configs;
 using Util.Datas.UnitOfWorks;
 
 namespace Util.Datas.Tests.Samples.Datas.SqlServer.UnitOfWorks {
     /// <summary>
     /// SqlServer工作单元
     /// </summary>
-    [Ignore]
     public class SqlServerUnitOfWork : Util.Datas.Ef.SqlServer.UnitOfWork, ISqlServerUnitOfWork {
         /// <summary>
         /// 初始化SqlServer工作单元
