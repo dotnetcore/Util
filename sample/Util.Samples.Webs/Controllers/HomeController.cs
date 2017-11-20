03864d34b02041ae141666fb56f4d3016b59d39e
utilcore <8907060@qq.com>
2017-11-20 21:26:40 +0800

    更新npm依赖库版本

--- sample/Util.Samples.Webs/Controllers/HomeController.cs
+++ sample/Util.Samples.Webs/Controllers/HomeController.cs
@@ -1,36 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Util.Datas.Tests.Samples.Datas.SqlServer.UnitOfWorks;
-using Util.Datas.Tests.Samples.Domains.Models;
-using Util.Datas.Tests.Samples.Domains.Repositories;
-using Util.Helpers;
 
 namespace Util.Samples.Webs.Controllers {
     public class HomeController : Controller {
 
-        public HomeController( IOrderRepository orderRepository, ICustomerRepository customerRepository, ISqlServerUnitOfWork unitOfWork ) {
-            _orderRepository = orderRepository;
-            _customerRepository = customerRepository;
-            _unitOfWork = unitOfWork;
+        public HomeController() {
         }
 
-        private ISqlServerUnitOfWork _unitOfWork;
-        private IOrderRepository _orderRepository;
-        private ICustomerRepository _customerRepository;
-
         public IActionResult Index( string id ) {
-            var customer = new Customer();
-            customer.Init();
-            customer.Balance = 18.52M;
-            customer.Name = Id.ObjectId().Substring( 0,18 );
-            _customerRepository.Add( customer );
-
-            var order = new Order();
-            order.Code = "code";
-            order.Name = "a";
-            order.Init();
-            _orderRepository.Add( order );
-
-            _unitOfWork.Commit();
             return View();
         }
 
