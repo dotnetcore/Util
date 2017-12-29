db3f0159a099efae2c278e83fa06bcfa12fe3cc3
utilcore <8907060@qq.com>
2017-12-29 22:31:28 +0800

    添加pgsql测试用例

---
+++ test/Util.Datas.Tests.Integration/PgSql/Configs/GlobalConfig.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Util.Datas.Tests.PgSql.Configs {
+    /// <summary>
+    /// 全局测试配置
+    /// </summary>
+    [CollectionDefinition( "PgSqlGlobalConfig" )]
+    public class GlobalConfig : ICollectionFixture<GlobalFixture> {
+    }
+}
--- test/Util.Datas.Tests.Integration/Samples/Domains/Models/Customer.cs
+++ test/Util.Datas.Tests.Integration/Samples/Domains/Models/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Util.Domains;
 using Util.Domains.Auditing;
 
