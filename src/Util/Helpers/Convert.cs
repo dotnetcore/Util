e8b8fa93336bdadf92f81ab24eda945dafc10659
何镇汐 <8907060@qq.com>
2018-02-27 12:44:05 +0800

    更新类库版本号

--- src/Util/Helpers/Convert.cs
+++ src/Util/Helpers/Convert.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Util.Helpers {
     /// <summary>
@@ -256,6 +257,23 @@ namespace Util.Helpers {
             catch {
                 return default( T );
             }
+        }
+
+        /// <summary>
+        /// 转换为字节数组
+        /// </summary>
+        /// <param name="input">输入值</param>        
+        public static byte[] ToBytes( string input ) {
+            return ToBytes( input, Encoding.UTF8 );
+        }
+
+        /// <summary>
+        /// 转换为字节数组
+        /// </summary>
+        /// <param name="input">输入值</param>
+        /// <param name="encoding">字符编码</param>
+        public static byte[] ToBytes( string input, Encoding encoding ) {
+            return string.IsNullOrWhiteSpace( input ) ? new byte[] { } : encoding.GetBytes( input );
         }
     }
 }
