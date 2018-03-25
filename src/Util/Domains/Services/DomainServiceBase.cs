d7ba209398fd96986545e8a9d13672688277ddd9
何镇汐 <8907060@qq.com>
2018-03-25 09:13:42 +0800

    增加领域服务基类

---
+++ src/Util/Domains/Services/DomainServiceBase.cs
@@ -0,0 +1,28 @@
+﻿using Util.Domains.Sessions;
+using Util.Logs;
+using Util.Logs.Core;
+
+namespace Util.Domains.Services {
+    /// <summary>
+    /// 领域服务
+    /// </summary>
+    public class DomainServiceBase : IDomainService {
+        /// <summary>
+        /// 初始化领域服务
+        /// </summary>
+        public DomainServiceBase() {
+            Logger = NullLog.Instance;
+            Session = Util.Domains.Sessions.Session.Null;
+        }
+
+        /// <summary>
+        /// 日志
+        /// </summary>
+        public ILog Logger { get; set; }
+
+        /// <summary>
+        /// 用户会话
+        /// </summary>
+        public ISession Session { get; set; }
+    }
+}
