b24b433ea5f3448c297c48917656cd2f68be97d4
何镇汐 <8907060@qq.com>
2018-05-08 12:04:00 +0800

    添加实体列表比较操作

---
+++ src/Util/Extensions.Domain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Util.Domains;
+
+namespace Util {
+    /// <summary>
+    /// 实体扩展
+    /// </summary>
+    public static partial class Extensions {
+        /// <summary>
+        /// 比较
+        /// </summary>
+        /// <typeparam name="TEntity">实体类型</typeparam>
+        /// <param name="newList">新实体集合</param>
+        /// <param name="oldList">旧实体集合</param>
+        public static ListCompareResult<TEntity, Guid> Compare<TEntity>( this IEnumerable<TEntity> newList, IEnumerable<TEntity> oldList )
+            where TEntity : IKey<Guid> {
+            return Compare<TEntity,Guid>( newList, oldList );
+        }
+
+        /// <summary>
+        /// 比较
+        /// </summary>
+        /// <typeparam name="TEntity">实体类型</typeparam>
+        /// <typeparam name="TKey">标识类型</typeparam>
+        /// <param name="newList">新实体集合</param>
+        /// <param name="oldList">旧实体集合</param>
+        public static ListCompareResult<TEntity, TKey> Compare<TEntity, TKey>( this IEnumerable<TEntity> newList, IEnumerable<TEntity> oldList )
+            where TEntity : IKey<TKey> {
+            var comparator = new ListComparator<TEntity, TKey>();
+            return comparator.Compare( newList, oldList );
+        }
+    }
+}
