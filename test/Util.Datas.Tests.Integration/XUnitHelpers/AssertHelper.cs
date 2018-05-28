7306d78ace1fc373ba85b2a7897499157e1830d2
utilcore <8907060@qq.com>
2018-05-28 09:43:21 +0800

    更新AspectCore版本到0.5

--- test/Util.Datas.Tests.Integration/XUnitHelpers/AssertHelper.cs
+++ test/Util.Datas.Tests.Integration/XUnitHelpers/AssertHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using AspectCore.DynamicProxy;
 using Xunit;
 
@@ -30,6 +31,32 @@ namespace Util.Datas.Tests.XUnitHelpers {
             }
             catch {
                 return Assert.Throws<TException>( action );
+            }
+        }
+
+        /// <summary>
+        /// 抛出异常，并从异常消息中搜索特定关键字
+        /// </summary>
+        /// <typeparam name="TException">异常类型</typeparam>
+        /// <param name="action">操作</param>
+        /// <param name="keyword">关键字</param>
+        public static async Task<TException> ThrowsAsync<TException>( Func<Task> action, string keyword = "" ) where TException : Exception {
+            var exception = await GetExceptionAsync<TException>( action );
+            if( !string.IsNullOrWhiteSpace( keyword ) )
+                Assert.Contains( keyword, exception.Message );
+            return exception;
+        }
+
+        /// <summary>
+        /// 获取异常
+        /// </summary>
+        private static async Task<TException> GetExceptionAsync<TException>( Func<Task> action ) where TException : Exception {
+            try {
+                var exception = await Assert.ThrowsAnyAsync<AspectInvocationException>( action );
+                return (TException)exception.InnerException;
+            }
+            catch {
+                return await Assert.ThrowsAsync<TException>( action );
             }
         }
     }
