15ce2505902e8dad367087f5c7d2d023f2c79e75
何镇汐 <8907060@qq.com>
2018-08-14 18:51:05 +0800

    更新ckeditor为4.8 full版本

--- sample/Util.Samples.Webs/wwwroot/js/ckeditor/config.js
+++ sample/Util.Samples.Webs/wwwroot/js/ckeditor/config.js
@@ -1,40 +1,17 @@
 /**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  * 配置文档：https://docs.ckeditor.com/ckeditor4/latest/api/CKEDITOR_config.html
  */
 
-CKEDITOR.editorConfig = function (config) {
+CKEDITOR.editorConfig = function( config ) {
     //皮肤
     config.skin = 'moono-lisa';
 
     //颜色
     config.uiColor = '#ffffff';
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' }
-	];
-
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
+    //移除上传图片的高级选项卡
     config.removeDialogTabs = 'image:advanced;link:advanced';
+    config.image_previewText = ' ';
 };
