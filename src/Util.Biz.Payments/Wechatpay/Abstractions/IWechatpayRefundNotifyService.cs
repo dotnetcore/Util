575b94ac5daee49b2cd86006ef755dca30ea9224
何镇汐 <8907060@qq.com>
2019-04-11 14:09:18 +0800

    重构微信退款回调通知服务

--- src/Util.Biz.Payments/Wechatpay/Abstractions/IWechatpayRefundNotifyService.cs
+++ src/Util.Biz.Payments/Wechatpay/Abstractions/IWechatpayRefundNotifyService.cs
@@ -1,11 +1,17 @@
 ﻿using Util.Biz.Payments.Core;
 
-namespace Util.Biz.Payments.Wechatpay.Abstractions
-{
+namespace Util.Biz.Payments.Wechatpay.Abstractions {
     /// <summary>
     /// 微信退款回调服务
     /// </summary>
-    public interface IWechatpayRefundNotifyService: INotifyService
-    {
+    public interface IWechatpayRefundNotifyService : INotifyService {
+        /// <summary>
+        /// 微信退款单号
+        /// </summary>
+        string RefundId { get; }
+        /// <summary>
+        /// 商户退款单号
+        /// </summary>
+        string RefundNo { get; }
     }
 }
--- src/Util.Biz.Payments/Wechatpay/Services/WechatpayRefundNotifyService.cs
+++ src/Util.Biz.Payments/Wechatpay/Services/WechatpayRefundNotifyService.cs
@@ -1,28 +1,26 @@
 ﻿using Util.Biz.Payments.Wechatpay.Abstractions;
 using Util.Biz.Payments.Wechatpay.Configs;
 
-namespace Util.Biz.Payments.Wechatpay.Services
-{
+namespace Util.Biz.Payments.Wechatpay.Services {
     /// <summary>
     /// 微信退款回调服务
     /// </summary>
-    public class WechatpayRefundNotifyService:WechatpayNotifyService,IWechatpayRefundNotifyService
-    {
+    public class WechatpayRefundNotifyService : WechatpayNotifyService, IWechatpayRefundNotifyService {
         /// <summary>
         /// 初始化微信退款回调服务
         /// </summary>
-        /// <param name="configProvider"></param>
-        public WechatpayRefundNotifyService(IWechatpayConfigProvider configProvider) : base(configProvider)
-        {
+        /// <param name="configProvider">配置提供器</param>
+        public WechatpayRefundNotifyService( IWechatpayConfigProvider configProvider ) : base( configProvider ) {
         }
+
         /// <summary>
         /// 微信退款单号
         /// </summary>
-        public string RefundId => GetParam("refund_id");
+        public string RefundId => GetParam( "refund_id" );
 
         /// <summary>
         /// 商户退款单号
         /// </summary>
-        public string RefundNo=> GetParam("out_refund_no");
+        public string RefundNo => GetParam( "out_refund_no" );
     }
 }
