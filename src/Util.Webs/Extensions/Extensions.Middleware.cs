05234362aa6c6d7d28da35d804b7357d873aadf9
jian玄冰 <jianxuanhuo1@126.com>
2019-07-03 14:30:08 +0800

    新增真实IP获取

--- src/Util.Webs/Extensions/Extensions.Middleware.cs
+++ src/Util.Webs/Extensions/Extensions.Middleware.cs
@@ -13,5 +13,13 @@ namespace Util.Webs.Extensions {
         public static IApplicationBuilder UseErrorLog( this IApplicationBuilder builder ) {
             return builder.UseMiddleware<ErrorLogMiddleware>();
         }
+
+        /// <summary>
+        /// 注册真实Ip中间件
+        /// </summary>
+        /// <param name="builder">应用程序生成器</param>
+        public static IApplicationBuilder UseRealIp(this IApplicationBuilder builder) {
+            return builder.UseMiddleware<RealIpMiddleware>();
+        }
     }
 }
