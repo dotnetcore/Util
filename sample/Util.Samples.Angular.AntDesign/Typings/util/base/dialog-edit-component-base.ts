5eb07ef18e6e19d09907ca62d38334d2b8d18841
何镇汐 <8907060@qq.com>
2019-07-27 07:54:31 +0800

    ng-zorro CRUD弹出层基类添加提交前和提交后回调函数

--- sample/Util.Samples.Angular.AntDesign/Typings/util/base/dialog-edit-component-base.ts
+++ sample/Util.Samples.Angular.AntDesign/Typings/util/base/dialog-edit-component-base.ts
@@ -30,7 +30,24 @@ export abstract class DialogEditComponentBase<TViewModel extends ViewModel> exte
             data: this.model,
             form: form,
             button: button,
-            closeDialog: true
+            closeDialog: true,
+            before: data => this.submitBefore( data ),
+            ok: result => this.submitAfter( result )
         } );
+    }
+
+    /**
+     * 提交前操作
+     * @param data 参数
+     */
+    protected submitBefore( data ) {
+        return true;
+    }
+
+    /**
+     * 提交后操作
+     * @param result 结果
+     */
+    protected submitAfter( result ) {
     }
 }
