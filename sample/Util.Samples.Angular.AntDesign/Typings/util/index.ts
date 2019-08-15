d6a11b041c80762854f53a5b4a5e4c2a66899bd8
何镇汐 <8907060@qq.com>
2019-08-15 20:59:10 +0800

    修复ng-zorro表格编辑指令保存时删除最后一页刷新不正确的bug

--- sample/Util.Samples.Angular.AntDesign/Typings/util/index.ts
+++ sample/Util.Samples.Angular.AntDesign/Typings/util/index.ts
@@ -1,7 +1,7 @@
 ﻿//============== util操作=========================
 //Copyright 2019 何镇汐
 //Licensed under the MIT license
-//Version: 1.0.1.9
+//Version: 1.0.2.0
 //================================================
 export { Util as util } from './util';
 export { UtilModule, createOidcProviders } from './util.module';
